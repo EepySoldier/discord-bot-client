7738398f08bd04a64b102eafac94901f31aff161
Eepy Soldier <szymon.tokarz2@gmail.com>
2025-07-21 23:59:58 +0200

    Image Owner

--- src/App.jsx
+++ src/App.jsx
@@ -106,6 +106,7 @@ function Home({user}) {
                         <div className="video-card" key={video.id}>
                             <h3>{video.activity_name}</h3>
                             <p className="upload-date">{new Date(video.upload_date).toLocaleString()}</p>
+                            <p className="video-owner">{video.video_owner}</p>
                             <div className="video-wrapper">
                                 <video width="100%" controls preload="metadata">
                                     <source src={video.file_url} type="video/mp4"/>
